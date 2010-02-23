8f1c3e656e5918059c9a13f296f2d253977b8e19
Samuel Leeman-Munk <samuel@Chumbo.(none)>
2010-02-23 13:52:23 -0500

    Added shell script for changing slashes in all #include statements to forwardslashes

---
+++ fixslashes.sh
@@ -0,0 +1,20 @@
+if [ $1 ] #Get the directory
+then
+DIRECTORY=$1 
+fi
+
+script='if ($_ =~ m/^#include/)
+  {
+          $_ =~ s/\\/\//g;
+  }
+  print $_;' #make a perl script
+
+echo $script > temp.pl #save it in a perl file to execute
+
+#Find -o wasn't working properly for some reason
+#Run script on all .cpp and .h files
+#in the given directory and its subdirectories
+find $DIRECTORY -name "*.cpp" -exec perl -ni temp.pl  '{}' \; -print
+find $DIRECTORY -name "*.h" -exec perl -ni temp.pl '{}' \; -print
+
+rm temp.pl # remove the script 
